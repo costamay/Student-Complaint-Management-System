8e7929031e288429c96bd8da4a743d0aa1df004b
Titus Ouko <titusouko@gmail.com>
2021-04-16 11:22:38 +0300

    Update config.php

--- users/includes/config.php
+++ users/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'cms');
+define('DB_SERVER','grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER','yy7ba6oxj8qw88st');
+define('DB_PASS' ,'xojgkshgmzfuxtax');
+define('DB_NAME', 'im1zjxkplab5dk1r');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
